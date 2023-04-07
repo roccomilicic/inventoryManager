b6597fad5b8b73106a88d8e086978da59c9fe641
Rocco <113945097+roccomilicic@users.noreply.github.com>
2023-04-07 20:15:50 +1200

    Update Inventory.java

--- src/inventorymanagement/Inventory.java
+++ src/inventorymanagement/Inventory.java
@@ -1,1 +1,38 @@
+package inventorymanagent;
 
+/**
+ *
+ * @author rocco
+ */
+import java.util.HashMap;
+
+public class Inventory {
+
+    private Product prodToAdd;
+    private int productQuantity;
+    private HashMap<int, Product> inventory;
+
+    public Inventory() {
+        this.inventory = new HashMap<int, Product>();
+    }
+
+    public Inventory(Product product) {
+        this.prodToAdd = product;
+        this.productToDelete = product;
+        this.productToUpdate = product;
+        this.inventory = new HashMap<int, Product>();
+    }
+
+    public Product addProduct(Product product) {
+        inventory.put(prodToAdd.getProductID(), prodToAdd);
+    }
+
+    public Product removeProduct(Product product) {
+        inventory.remove(prodToAdd.getProductID(), prodToAdd);
+    }
+
+    public Product updateProduct(Product product) {
+
+    }
+
+}
