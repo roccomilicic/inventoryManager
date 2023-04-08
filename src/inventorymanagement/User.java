a40f3ea844175917d39926b5c784b1f093fe9452
Rocco <113945097+roccomilicic@users.noreply.github.com>
2023-04-09 00:21:39 +1200

    Update User.java

--- src/inventorymanagement/User.java
+++ src/inventorymanagement/User.java
@@ -1,14 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package inventorymanagement;
 
 /**
  *
- * @author rocco
+ * @author Rocco + Beedrix
  */
 public class User {
     private int userID;
