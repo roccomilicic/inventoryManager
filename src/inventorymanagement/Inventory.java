8d04e17ef3dcf8ae9b092c5bb675e5927492eb86
Rocco <113945097+roccomilicic@users.noreply.github.com>
2023-04-09 00:20:18 +1200

    Update Inventory.java
    Created a toString (not finished) and started working on the addProduct method (But the product toString to display a list of products will not print)

--- src/inventorymanagement/Inventory.java
+++ src/inventorymanagement/Inventory.java
@@ -1,38 +1,58 @@
-package inventorymanagent;
+package inventorymanagement;
 
 /**
  *
- * @author rocco
+ * @author Rocco + Beedrix
  */
+
 import java.util.HashMap;
 
 public class Inventory {
 
-    private Product prodToAdd;
+    Product product = new Product(); // to call product methods
     private int productQuantity;
-    private HashMap<int, Product> inventory;
+    private HashMap<Integer, Product> inventory;
 
-    public Inventory() {
-        this.inventory = new HashMap<int, Product>();
+    public Inventory() { // default constructor
+        this.inventory = new HashMap<Integer, Product>();
     }
 
-    public Inventory(Product product) {
+    /*public Inventory(Product product) {
         this.prodToAdd = product;
         this.productToDelete = product;
         this.productToUpdate = product;
-        this.inventory = new HashMap<int, Product>();
+        this.inventory = new HashMap<Integer, Product>();
+    }*/
+
+    public void addProduct() {
+        // Adds the users choice of product to the current inventory
+        System.out.println(product.toString()); // at the moment its not printing the toString????? but it can print it from UserService main
+        
+        //inventory.put(prodToAdd.getProductID(), prodToAdd);
     }
 
-    public Product addProduct(Product product) {
-        inventory.put(prodToAdd.getProductID(), prodToAdd);
-    }
-
-    public Product removeProduct(Product product) {
+    /*public Product removeProduct(Product product) {
         inventory.remove(prodToAdd.getProductID(), prodToAdd);
     }
 
     public Product updateProduct(Product product) {
 
-    }
+    }*/
 
+    @Override
+    public String toString() {
+        String output = "\n╔══════════════════════════════╗";
+        output += "\n║                  INVENTORY:                    ║";
+        output += ("\n╚══════════════════════════════╝");
+        
+
+        /*for (HashMap.Entry<Integer, Product> inventoryItem : inventory.entrySet()) {
+            int id = inventoryItem.getKey();
+            Product product = inventoryItem.getValue();
+
+            output += "\nID: %d\n%s\n", id, product.toString()));
+        }*/
+
+        return output;
+    }
 }
