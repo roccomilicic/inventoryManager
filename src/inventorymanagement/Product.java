ac5f7423699855694ae28238b8f95956d13cbc84
Rocco <113945097+roccomilicic@users.noreply.github.com>
2023-04-09 00:21:16 +1200

    Adding toString to print out all products
    Plus a temporary generateProducts method for sample products

--- src/inventorymanagement/Product.java
+++ src/inventorymanagement/Product.java
@@ -1,22 +1,22 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package inventorymanagement;
 
 /**
  *
- * @author rocco
+ * @author Rocco + Beedrix
  */
+import java.util.ArrayList;
+
 public class Product {
     private int productID;
     private String productName;
-    private int price;
-    private ArrayList<Product> products = new ArrayList<Product>();
+    private double price;
+    private ArrayList<Product> products;
 
-    public Product(int productID, String productName, int price, int quantity) {
+    public Product() {
+        this.products = new ArrayList<Product>();
+    }
+
+    public Product(int productID, String productName, double price) {
         this.productID = productID;
         this.productName = productName;
         this.price = price;
@@ -36,10 +36,48 @@ public class Product {
         this.productName = productName;
     }
 
-    public int getPrice() {
+    public double getPrice() {
         return price;
     }
-    public void setPrice(int price) {
+    public void setPrice(double price) {
         this.price = price;
+    }
+
+    public void generateProducts() {
+        // temporary? method which creates ample products and adds them to products list
+        products.add(new Product(1, "Apples", 0.60));
+        products.add(new Product(2, "Banana", 0.65));
+        products.add(new Product(3, "Orange", 0.70));
+        products.add(new Product(4, "Avocado", 1.10));
+        products.add(new Product(5, "Kiwifruit", 0.85));
+        products.add(new Product(6, "Feijoa", 0.55));
+        products.add(new Product(7, "Pineapple", 4.20));
+        products.add(new Product(8, "Tomato", 0.40));
+        products.add(new Product(9, "Coconut", 4.50));
+        products.add(new Product(10, "Pear", 0.75));
+    }
+
+    /*@Override
+    public String toString() {
+        String output = "╔══════════════════════════════╗";
+        output += "\n║                  PRODUCT LIST:                 ║";
+        output += ("\n╚══════════════════════════════╝");
+        
+        for (Product product : products) {
+            output += product;
+        }
+
+        return output;
+    }*/
+
+    @Override
+    public String toString() {
+        String output = "";
+        
+        for (Product product : products) {
+            output += "ID: " + product.getProductID() + " PRODUCT: " + product.getProductName() + " PRICE: " + product.getPrice() + "\n";
+        }
+
+        return output;
     }
 }
