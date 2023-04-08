9047ed5e6db27d04aa145797a048ed06e4323ddc
Rocco <113945097+roccomilicic@users.noreply.github.com>
2023-04-09 00:22:14 +1200

    Started working on user menus

--- src/inventorymanagement/UserService.java
+++ src/inventorymanagement/UserService.java
@@ -1,33 +1,107 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package inventorymanagement;
 
 /**
  *
- * @author rocco
+ * @author Rocco + Beedrix
  */
+
+import java.util.Scanner;
+
 public class UserService {
+    
+    Scanner scan = new Scanner(System.in);
+    //private Inventory inventory;
+    //private UserService userServ;
+
+    /*public UserService() {
+        Inventory inventory = = new Inventory();
+        UserService userServ = new UserService(); 
+    }*/
+
+    public void printUserMenu() {
+        // Prints the user menu
+        System.out.println("╔══════════════════════════════╗");
+        System.out.println("║               INVENTORY MANAGER                ║");
+        System.out.println("╠══════════════════════════════╣");
+        System.out.println("║ 1. View inventory                              ║");
+        System.out.println("║ 2. Print inventory to TXT file                 ║");
+        System.out.println("║ 3. Update inventory                            ║");
+        System.out.println("║ 4. Exit program                                ║");
+        System.out.println("╚══════════════════════════════╝");
+    }
+    public void userMenu(Inventory inventory) {
+        // Gets the user input for the update inventory menu
+        printUserMenu();
+
+        System.out.println("SELECT: ");
+        int selectedMenuOption = scan.nextInt();
+       
+        switch (selectedMenuOption) {
+            case 1:
+                System.out.println(inventory.toString());
+                break;
+            case 2:
+                //printInventoryToFile();
+                break;
+            case 3:
+                updateInventory(inventory);
+                break;
+            case 4:
+                System.out.println("> Exiting Inventory Manager...");
+                System.exit(0);
+                break;
+            default:
+                System.out.println("> Invalid choice. Please try again.");
+        }
+        System.out.println(); // newline for character formatting
+    }
+
+    public void printUpdateInventory() {
+        // Prints the update inventory menu
+        System.out.println("╔══════════════════════════════╗");
+        System.out.println("║               UPDATE INVENTORY                 ║");
+        System.out.println("╠══════════════════════════════╣");
+        System.out.println("║ 1. Add product to inventory                    ║");
+        System.out.println("║ 2. Remove product from inventory               ║");
+        System.out.println("║ 3. Update inventory                            ║");
+        System.out.println("║ 4. Go back                                     ║");
+        System.out.println("╚══════════════════════════════╝");
+    }
+
+    public void updateInventory(Inventory inventory) {
+        // Gets the user input for the update inventory menu 
+        printUpdateInventory();
+
+        System.out.println("SELECT: ");
+        int updateInventoryOption = scan.nextInt();
+       
+        switch (updateInventoryOption) {
+            case 1:
+                inventory.addProduct();
+                break;
+            case 2:
+
+                break;
+            case 3:
+
+                //break;
+            case 4:
+                System.out.println("> Back to menu");
+                userMenu(inventory);
+                break;
+            default:
+                System.out.println("> Invalid choice. Please try again.");
+        }
+        System.out.println(); // newline for character formatting*/
+    }
 
     public static void main(String[] args) {
+        Inventory inventory = new Inventory(); // calls default constructor to acces methods
+        Product product = new Product(); // calls default constructor to acces methods
+        UserService userServ = new UserService(); 
 
-        products.add(new Product(1, "Apples", 0.60))
-        products.add(new Product(2, "Banana", 0.65))
-        products.add(new Product(3, "Orange", 0.70))
-        products.add(new Product(4, "Avocado", 1.10))
-        products.add(new Product(5, "Kiwifruit", 0.85))
-        products.add(new Product(6, "Feijoa", 0.55))
-        products.add(new Product(7, "Pineapple", 4.20))
-        products.add(new Product(8, "Tomato", 0.40))
-        products.add(new Product(9, "Coconut", 4.50))
-        products.add(new Product(10, "Pear", 0.75))
-
-        Inventory inventory1 = new Inventory();
-
-        inventory1.addProduct
-
+        product.generateProducts(); // creates sample products for user to add/remove
+        System.out.println(product.toString());
+        userServ.userMenu(inventory); // starts off the program (would come after user login once implemented)
     }
 }
