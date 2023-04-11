f12358091483b0bd371077814a72563104143339
Rocco <113945097+roccomilicic@users.noreply.github.com>
2023-04-11 21:32:28 +1200

    Moved things to ProductList and added quantity var

--- src/inventorymanagement/Product.java
+++ src/inventorymanagement/Product.java
@@ -4,22 +4,21 @@ package inventorymanagement;
  *
  * @author Rocco + Beedrix
  */
-import java.util.ArrayList;
 
 public class Product {
     private int productID;
     private String productName;
     private double price;
-    private ArrayList<Product> products;
+    private int quantity;
 
     public Product() {
-        this.products = new ArrayList<Product>();
     }
 
     public Product(int productID, String productName, double price) {
         this.productID = productID;
         this.productName = productName;
         this.price = price;
+        this.quantity = quantity;
     }
 
     public int getProductID() {
@@ -43,40 +42,14 @@ public class Product {
         this.price = price;
     }
 
-    public Product getProductById(int productId) {
-    // To search for a product object with just the ID (to add to inventory)
-        for (Product prod : products) {
-            if (prod.getProductID() == productId) {
-                return prod;
-            }
-        }
-        return null;
+    public int getQuantity() {
+        return quantity;
     }
-
-    public void generateProducts() {
-        // temporary? method which creates ample products and adds them to products list
-        products.add(new Product(1, "Apples", 0.60));
-        products.add(new Product(2, "Banana", 0.65));
-        products.add(new Product(3, "Orange", 0.70));
-        products.add(new Product(4, "Avocado", 1.10));
-        products.add(new Product(5, "Kiwifruit", 0.85));
-        products.add(new Product(6, "Feijoa", 0.55));
-        products.add(new Product(7, "Pineapple", 4.20));
-        products.add(new Product(8, "Tomato", 0.40));
-        products.add(new Product(9, "Coconut", 4.50));
-        products.add(new Product(10, "Pear", 0.75));
+    public void addQuantity(int quantity) { // adds onto the quantity of a product in inventory
+        this.quantity += quantity;
     }
-
-    @Override
-    public String toString() {
-        String output = "╔══════════════════════════════╗";
-        output += "\n║                  PRODUCT LIST:                 ║";
-        output += ("\n╚══════════════════════════════╝\n");
-        
-        for (Product product : products) {
-            output += "ID: " + product.getProductID() + "       PRODUCT: " + product.getProductName() + "       PRICE: " + product.getPrice() + "\n";
-        }
-
-        return output;
+    public void setQuantity(int quantity) { // overwrites the quantity of a product in inventory
+        this.quantity = quantity;
     }
 }
+
