334cfc6529dc9be2fd660dc3c5e007bb19ff4788
Rocco <113945097+roccomilicic@users.noreply.github.com>
2023-04-11 21:31:46 +1200

    + Add product, remove product, and updateQuantity
    All methods completed

--- src/inventorymanagement/Inventory.java
+++ src/inventorymanagement/Inventory.java
@@ -10,15 +10,17 @@ import java.util.Scanner;
 
 public class Inventory {
 
-    Product product;
-    UserService userServ = new UserService();
-    Scanner scan = new Scanner(System.in);
+    private Product product;
+    //private UserService userServ = new UserService();
+    ProductList productList;
+    private Scanner scan = new Scanner(System.in);
     private int productQuantity;
     private HashMap<Integer, Product> inventory;
 
     public Inventory() { // default constructor
         this.inventory = new HashMap<Integer, Product>();
-        this.product  = new Product(); // to call product methods
+        this.product = new Product();
+        this.productList = new ProductList(); // to call product methods
     }
 
     /*public Inventory(Product product) {
@@ -28,16 +30,17 @@ public class Inventory {
         this.inventory = new HashMap<Integer, Product>();
     }*/
 
-    public void addProduct() {
+    public void addProduct(ProductList productList) {
         // Adds the users choice of product to the current inventory
-        product.generateProducts(); // generates products to pick from
-        System.out.println(product.toString()); // 
-
-        System.out.println("SELECT AN ITEM BASED ON ID: ");
+        System.out.println("ADD AN ITEM BASED ON ID: ");
         int productToAdd = scan.nextInt();
-        product = product.getProductById(productToAdd); // selects product to add based on ID from user
+        product = productList.getProductById(productToAdd); // selects product to add based on ID from user
 
         if (product != null) { // if != empty product
+            System.out.println("HOW MANY WOULD YOU LIKE TO ADD: ");
+            int quantityToAdd = scan.nextInt();
+            product.addQuantity(quantityToAdd);
+
             inventory.put(product.getProductID(), product); // add product to inventory hashmap
             System.out.println("> Added " + product.getProductName() + " to inventory");
             
@@ -46,24 +49,48 @@ public class Inventory {
         }
     }
 
-    /*public Product removeProduct(Product product) {
-        inventory.remove(prodToAdd.getProductID(), prodToAdd);
+    public void removeProduct(ProductList productList) {
+        // Removes the users choice of product to the current inventory
+        System.out.println(toString());
+        System.out.println("\nREMOVE AN ITEM BASED ON ID: ");
+        int productToRemove = scan.nextInt();
+        product = productList.getProductById(productToRemove); // selects product to add based on ID from user
+
+        if (product != null && inventory.containsKey(productToRemove)) { // if != empty product and if product is in inventory
+            inventory.remove(productToRemove); // remove product from inventory hashmap
+            System.out.println("> Removed " + product.getProductName() + " from inventory");    
+        } else {
+            System.out.println("> Item not found!");
+        }
     }
 
-    public Product updateProduct(Product product) {
+    public void adjustQuantity(ProductList productList) {
+        // Updates the quantity of chosen product in the current inventory
+        System.out.println(toString());
+        System.out.println("\nSELECT ITEM TO UPDATE BASED ON ID: ");
+        int productToUpdate = scan.nextInt();
+        product = productList.getProductById(productToUpdate); // selects product to add based on ID from user
 
-    }*/
+        if (product != null && inventory.containsKey(productToUpdate)) { // if != empty product and if product is in inventory
+            System.out.println("ENTER THE NEW QUANTITY OF " + product.getProductName().toUpperCase() + "'S: ");
+            int newQuantity = scan.nextInt();
+            product.setQuantity(newQuantity); // overwrite the quanity based on user input
+            System.out.println("> Updated " + product.getProductName() + " to a quantity of " + product.getQuantity());  
+        } else {
+            System.out.println("> Item not found!");
+        }
+    }
 
     @Override
     public String toString() {
         String output = "\n╔══════════════════════════════╗";
         output += "\n║                  INVENTORY:                    ║";
-        output += ("\n╚══════════════════════════════╝");
+        output += ("\n╚══════════════════════════════╝\n");
         
 
         for (HashMap.Entry<Integer, Product> entry : inventory.entrySet()) {
             Product product = entry.getValue();
-            output += "\nID: " + product.getProductID() + ", Name: " + product.getProductName() + ", Price: " + product.getPrice() + "\n";
+            output += "ID: " + product.getProductID() + ", Name: " + product.getProductName() + ", Price: " + product.getPrice() + ", Quantity: " + product.getQuantity() + "\n";
         }
 
         return output;
