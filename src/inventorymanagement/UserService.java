0151ade78daf8ba11c841874c1a0f15242116def
Beedrick <103038493+Beedrick@users.noreply.github.com>
2023-04-17 23:54:28 +1200

    Update UserService.java

--- src/inventorymanagement/UserService.java
+++ src/inventorymanagement/UserService.java
@@ -8,7 +8,7 @@ package inventorymanagement;
 import java.util.Scanner;
 
 public class UserService {
-    
+
     Scanner scan = new Scanner(System.in);
     private ProductList productList;
     //private Inventory inventory;
@@ -25,21 +25,22 @@ public class UserService {
 
         productList.generateProducts();
 
-         while (true) { // loops menu till user quits the program
+        while (true) { // loops menu till user quits the program
             userServ.userMenu(inventory); // starts off the program (would come after user login once implemented)
         }
     }
 
-    public void printUserMenu() {
+    public static void printUserMenu() {
         // Prints the user menu
-        System.out.println("╔══════════════════════════════╗");
-        System.out.println("║               INVENTORY MANAGER                ║");
-        System.out.println("╠══════════════════════════════╣");
-        System.out.println("║ 1. View inventory                              ║");
-        System.out.println("║ 2. Print inventory to TXT file                 ║");
-        System.out.println("║ 3. Update inventory                            ║");
-        System.out.println("║ 4. Exit program                                ║");
-        System.out.println("╚══════════════════════════════╝");
+        System.out.printf("+-------------------------------------------------+\n");
+        System.out.printf("|                  INVENTORY MANAGER              |\n");
+        System.out.printf("+-------------------------------------------------+\n");
+        System.out.printf("| %-2s. %-42s  |\n", "1", "View inventory");
+        System.out.printf("| %-2s. %-42s  |\n", "2", "Print inventory to TXT file");
+        System.out.printf("| %-2s. %-42s  |\n", "3", "Update inventory");
+        System.out.printf("| %-2s. %-42s  |\n", "4", "Exit program");
+        System.out.printf("+-------------------------------------------------+\n");
+        System.out.print("Enter your choice: ");
     }
 
     public void userMenu(Inventory inventory) {
@@ -47,7 +48,7 @@ public class UserService {
         printUserMenu();
         System.out.println("SELECT: ");
         int selectedMenuOption = scan.nextInt();
-       
+
         switch (selectedMenuOption) {
             case 1:
                 System.out.println(inventory.toString());
@@ -69,14 +70,15 @@ public class UserService {
 
     public void printUpdateInventory() {
         // Prints the update inventory menu
-        System.out.println("╔══════════════════════════════╗");
-        System.out.println("║               UPDATE INVENTORY                 ║");
-        System.out.println("╠══════════════════════════════╣");
-        System.out.println("║ 1. Add product to inventory                    ║");
-        System.out.println("║ 2. Remove product from inventory               ║");
-        System.out.println("║ 3. Update quantity of a product                ║");
-        System.out.println("║ 4. Go back                                     ║");
-        System.out.println("╚══════════════════════════════╝");
+        System.out.printf("+-------------------------------------------------+\n");
+        System.out.printf("|                  UPDATE INVENTORY               |\n");
+        System.out.printf("+-------------------------------------------------+\n");
+        System.out.printf("| %-2s. %-43s |\n", "1", "Add product to inventory");
+        System.out.printf("| %-2s. %-43s |\n", "2", "Remove product from inventory");
+        System.out.printf("| %-2s. %-43s |\n", "3", "Update quantity of a product");
+        System.out.printf("| %-2s. %-43s |\n", "4", "Go back");
+        System.out.printf("+-------------------------------------------------+\n");
+        System.out.print("Enter your choice: ");
     }
 
     public void updateInventoryMenu(Inventory inventory) {
@@ -85,7 +87,7 @@ public class UserService {
 
         System.out.println("SELECT: ");
         int updateInventoryOption = scan.nextInt();
-       
+
         switch (updateInventoryOption) {
             case 1:
                 System.out.println(productList.toString()); // displays products available to add to inventory
@@ -107,10 +109,16 @@ public class UserService {
     }
 
     public static void main(String[] args) {
+        
+        LogIn logIn = new LogIn();
+        
+        logIn.logInInterface();
+        
         Inventory inventory = new Inventory(); // calls default constructor to acces methods
         //Product product = new Product(); // calls default constructor to acces methods
         UserService userServ = new UserService(); // calls default constructor to acces methods
 
-        userServ.startProgram(userServ, inventory);        
+        userServ.startProgram(userServ, inventory);
     }
 }
+
