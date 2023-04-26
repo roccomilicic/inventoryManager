a68dfb2a847741c359f799a456422c440a576722
Beedrick <103038493+Beedrick@users.noreply.github.com>
2023-04-26 20:05:40 +1200

    Update Product.java

--- src/inventorymanagement/Product.java
+++ src/inventorymanagement/Product.java
@@ -6,6 +6,7 @@ package inventorymanagement;
  */
 
 public class Product {
+
     private int productID;
     private String productName;
     private double price;
@@ -18,19 +19,27 @@ public class Product {
         this.productID = productID;
         this.productName = productName;
         this.price = price;
-        this.quantity = quantity;
     }
 
+    public Product(int productID, String productName, double price, int quantity){
+        this.productID = productID;
+        this.productName = productName;
+        this.price = price;
+        this.quantity = quantity;
+    }
+    
     public int getProductID() {
         return productID;
     }
+
     public void setProductID(int productID) {
-      this.productID = productID;
+        this.productID = productID;
     }
 
     public String getProductName() {
         return productName;
     }
+
     public void setProductName(String productName) {
         this.productName = productName;
     }
@@ -38,6 +47,7 @@ public class Product {
     public double getPrice() {
         return price;
     }
+
     public void setPrice(double price) {
         this.price = price;
     }
@@ -45,9 +55,11 @@ public class Product {
     public int getQuantity() {
         return quantity;
     }
+
     public void addQuantity(int quantity) { // adds onto the quantity of a product in inventory
         this.quantity += quantity;
     }
+
     public void setQuantity(int quantity) { // overwrites the quantity of a product in inventory
         this.quantity = quantity;
     }
