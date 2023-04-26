a6e3f0d86e0181ca48d37adb39cad8662d6a94f6
Beedrick <103038493+Beedrick@users.noreply.github.com>
2023-04-26 20:03:02 +1200

    Update Inventory.java
    Now the inventory automatically prints the inventory object to a txt file each time it is updated so the user no longer needs to manually update it.

--- src/inventorymanagement/Inventory.java
+++ src/inventorymanagement/Inventory.java
@@ -20,21 +20,58 @@ public class Inventory {
     ProductList productList;
     private int productQuantity;
     private HashMap<Integer, Product> inventory;
+    private String currentUser;
 
-    public Inventory() { // default constructor
+    public Inventory(String currentUser) { // default constructor
         this.inventory = new HashMap<Integer, Product>();
         this.product = new Product();
         this.productList = new ProductList(); // to call product methods
+        this.currentUser = currentUser;
+
+        setProductListArray();
     }
 
-    /*public Inventory(Product product) {
-        this.prodToAdd = product;
-        this.productToDelete = product;
-        this.productToUpdate = product;
-        this.inventory = new HashMap<Integer, Product>();
-    }*/
+    public void setProductListArray() {
+
+        File file = new File("UserProfiles/" + this.currentUser + '/' + this.currentUser + "Inventory.txt");
+
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+
+            if (file.length() != 0) {
+                String line;
+
+                while ((line = br.readLine()) != null) {
+                    // skip empty or whitespace-only lines
+                    if (line.trim().isEmpty()) {
+                        continue;
+                    }
+                    String[] parts = line.split(",");
+
+                    // parse values
+                    int id = Integer.parseInt(parts[0]);
+                    String name = parts[1];
+                    double price = Double.parseDouble(parts[2]);
+                    int quantity = Integer.parseInt(parts[3]);
+
+                    Product setProduct = new Product(id, name, price, quantity);
+                    this.inventory.put(id, setProduct);
+                }
+            } else {
+                this.inventory = new HashMap<Integer, Product>();
+            }
+
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+
+    public void setCurrentUser(String currentUser) {
+        this.currentUser = currentUser;
+    }
+
     public void addProduct(ProductList productList) {
         // Adds the users choice of product to the current inventory
+
         System.out.println("Add an item based on ID: ");
         int productToAdd = scan.nextInt();
         product = productList.getProductById(productToAdd); // selects product to add based on ID from user
@@ -46,7 +83,8 @@ public class Inventory {
 
             inventory.put(product.getProductID(), product); // add product to inventory hashmap
             System.out.println("> Added " + product.getProductName() + " to inventory");
-
+            inventoryToTxt();
+            
         } else {
             System.out.println("> Item not found!");
         }
@@ -54,7 +92,7 @@ public class Inventory {
 
     public void removeProduct(ProductList productList) {
         // Removes the users choice of product to the current inventory
-        if(inventory.isEmpty()) { // if no items inventory
+        if (inventory.isEmpty()) { // if no items inventory
             System.out.println("> You need at least 1 item in your inventory in order to remove an item.\n");
         } else { // if items in inventory
             System.out.println(toString());
@@ -66,6 +104,7 @@ public class Inventory {
                 inventory.remove(productToRemove); // remove product from inventory hashmap
                 product.setQuantity(0); // reset quanity incase added back in the future
                 System.out.println("> Removed " + product.getProductName() + " from inventory");
+                inventoryToTxt();
             } else {
                 System.out.println("> Item not found!");
             }
@@ -74,7 +113,7 @@ public class Inventory {
 
     public void adjustQuantity(ProductList productList) {
         // Updates the quantity of chosen product in the current inventory     
-        if(inventory.isEmpty()) { // if no items inventory
+        if (inventory.isEmpty()) { // if no items inventory
             System.out.println("> You need at least 1 item in your inventory in order to adjust the quantity of item.\n");
         } else { // if items in inventory
             System.out.println(toString());
@@ -87,6 +126,7 @@ public class Inventory {
                 int newQuantity = scan.nextInt();
                 product.setQuantity(newQuantity); // overwrite the quanity based on user input
                 System.out.println("> Updated " + product.getProductName() + " to a quantity of " + product.getQuantity());
+                inventoryToTxt();
             } else {
                 System.out.println("> Item not found!");
             }
@@ -95,24 +135,35 @@ public class Inventory {
 
     public void inventoryToTxt() {
         // Gets the current inventory and writes to a newly made txt file  
-        if(inventory.isEmpty()){
-            System.out.println("> You need at least 1 item in your inventory in order to save it as a txt file.\n");
-        } else { 
-            try {
-                System.out.println("What would you like to save the txt file as: ");
-                inventoryTxt = scan.next();
-                File inventoryFile = new File(inventoryTxt + ".txt");
+        //if (inventory.isEmpty()) {
+        // System.out.println("> You need at least 1 item in your inventory in order to save it as a txt file.\n");
+        //} else {
 
-                if (inventoryTxt != null) { // makes sure user actually entered a file name
-                    PrintWriter pw = new PrintWriter(new FileOutputStream(inventoryFile));
-                    pw.println(toString()); // Appends the inventory toString to the txt file
-                    System.out.println("> Inventory added to file '" + inventoryFile + "'");
-                    pw.close();
-                }
-            } catch (FileNotFoundException e) {
-                System.out.println(e.getMessage());
+        try {
+
+            // makes sure user actually entered a file name
+            File newUserInven = new File("UserProfiles/" + this.currentUser + "/" + this.currentUser + "Inventory.txt");
+            PrintWriter pw = new PrintWriter(new FileOutputStream(newUserInven));
+
+            String output = "";
+            for (HashMap.Entry<Integer, Product> entry : inventory.entrySet()) {
+                Product product = entry.getValue();
+                output += String.format(product.getProductID() + "," + product.getProductName() + ","
+                        + product.getPrice() + "," + product.getQuantity() + "\n");
             }
+
+            pw.println(output); // Appends the inventory toString to the txt file
+            System.out.println("> Inventory added to file '" + newUserInven + "'");
+            pw.close();
+
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
         }
+        //}
+    }
+
+    public String getInventoryTxt() {
+        return this.inventoryTxt;
     }
 
     @Override
@@ -120,23 +171,23 @@ public class Inventory {
 
         String output = "";
 
-        if(inventory.isEmpty()){
-            output = "> You currently have no items in your inventory.\n";
-        } else {
-            output += "+-------------------------------------------------------------+\n";
-            output += "|                        INVENTORY:                           |\n";
-            output += "|-------------------------------------------------------------|\n";
-            output += "| ID     | Name              | Price         | Quantity       |\n";
-            output += "+-------------------------------------------------------------+\n";
+        //if (inventory.isEmpty()) {
+        //     output = "> You currently have no items in your inventory.\n";
+        // } else {
+        output += "+-------------------------------------------------------------+\n";
+        output += "|                        INVENTORY:                           |\n";
+        output += "|-------------------------------------------------------------|\n";
+        output += "| ID     | Name              | Price         | Quantity       |\n";
+        output += "+-------------------------------------------------------------+\n";
 
-            for (HashMap.Entry<Integer, Product> entry : inventory.entrySet()) {
-                Product product = entry.getValue();
-                output += String.format("| %-3d    | %-10s        | $%-6.2f       | %-6d         |\n",
-                        product.getProductID(), product.getProductName(), product.getPrice(), product.getQuantity());
-            }
-
-            output += "+-------------------------------------------------------------+\n";
+        for (HashMap.Entry<Integer, Product> entry : inventory.entrySet()) {
+            Product product = entry.getValue();
+            output += String.format("| %-3d    | %-10s        | $%-6.2f       | %-6d         |\n",
+                    product.getProductID(), product.getProductName(), product.getPrice(), product.getQuantity());
         }
+
+        output += "+-------------------------------------------------------------+\n";
+        //}
 
         return output;
     }
